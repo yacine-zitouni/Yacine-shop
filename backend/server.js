ca35cdfd5e028f0694f821dbacda057cdc05f0d8
yaciiinee <yac.zitouni1962@gmail.com>
2021-10-19 17:30:43 +0100

    prepare to deployement

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,9 @@ const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "frontend/build")));
-  app.get("*", (req, res) => res.sendFile("index.html"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
 }
 
 const port = process.env.PORT || 5000;
