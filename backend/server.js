2df06e74fa0416b7a46747c0590a433a5c392b7d
yaciiinee <yac.zitouni1962@gmail.com>
2021-10-19 07:39:51 +0100

    prepare for deployemnt

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use("/api/users", userRouter);
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "frontend/build")));
   app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html")))
 }
 
